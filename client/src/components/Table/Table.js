b6ed1f22b0f54705a589dcbf074629733e4b463d
Zak Korme <zakariya.korme@gmail.com>
2021-02-12 16:40:40 -0500

    added Modal to get users bet

--- client/src/components/Table/Table.js
+++ client/src/components/Table/Table.js
@@ -4,82 +4,146 @@ import Deck from "../../util/deck";
 import Card from "../Card/Card";
 import classes from "./Table.module.css";
 import { useState } from "react";
+import Modal from "react-modal";
 
-
+Modal.setAppElement("#root");
+const modalStyle = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
 
 const Table = (props) => {
-    
-    const [dealerCards, setDealerCards] = useState([]);
-    const [playerCards, setPlayerCards] = useState([]);
-    let players = props.players < 2 ? <Player playerNum={1}/>:(
-        <div style={{paddingTop: "100px"}}>
-            <Player playerNum={1}/>
-            <Player playerNum={2}/>
-        </div>
-    ); 
-    let dealer = new Deck();
-    let player1 = new Deck();
-    dealer.shuffle();
-    player1.shuffle();
-    
-    const hitHandler = () => {
-        let hitCard = player1.deal();
-        setPlayerCards(playerCards => [...playerCards, <Card key={`player-${playerCards.len+1}`} suit={hitCard[0]} number={hitCard[1]}/>]);
-        
-    };
+  const [dealerCards, setDealerCards] = useState([]);
+  const [playerCards, setPlayerCards] = useState([]);
+  const [disableBtn, setDisableBtn] = useState(false);
+  const [showModal, setShowModal] = useState(true);
 
-    const countCards = (arr) => {
-        let num = 0;
-        arr.map(card => num += card.props.number);
-        return num;
-    };
-
-    const isWinner = () => {
-        let playerHand = countCards(playerCards);
-        let dealerHand = countCards(dealerCards);
-        
-        if (playerHand > dealerHand && playerHand <= 21) {
-            console.log("YOU WON!");
-            console.log("player:", playerHand);
-            console.log("dealer:", dealerHand);
-        } else {
-
-            console.log("Sorry, dealer won...");
-            console.log("player:", playerHand);
-            console.log("dealer:", dealerHand);
-        }
-
-    };
-
-    if (playerCards.length < 2) {
-
-        for (let i = 0; i < 2; i++) {
-            let temp = player1.deal();
-            setPlayerCards(playerCards => [...playerCards, <Card key={`player-${i} + ${temp[0]}`} suit={temp[0]} number={temp[1]}/>]);
-        };
-        
-        for (let i = 0; i < 2; i++) {
-            let temp = dealer.deal();
-            setDealerCards(dealerCards => [...dealerCards, <Card key={`dealer-${i} + ${temp[0]}`} suit={temp[0]} number={temp[1]}/>]);
-        };
-    };
-
-
-    return (
-        <div className={classes.Table}>
-            <Dealer/>
-            <div>
-                {dealerCards}
-            </div>
-            {players}
-            <div>
-                {playerCards}
-            </div>
-            <h4 style={{textAlign: "right"}}>Amount: ${props.bet}</h4>
-            <button className={classes.Btn} onClick={hitHandler}>Hit</button>
-            <button className={classes.Btn} onClick={isWinner}>Pass</button>
-        </div>
+  let players =
+    props.players < 2 ? (
+      <Player playerNum={1} />
+    ) : (
+      <div style={{ paddingTop: "100px" }}>
+        <Player playerNum={1} />
+        <Player playerNum={2} />
+      </div>
     );
-}
+  let dealer = new Deck();
+  let player1 = new Deck();
+  dealer.shuffle();
+  player1.shuffle();
 
-export default Table
+  const hitHandler = () => {
+    let hitCard = player1.deal();
+    setPlayerCards((playerCards) => [
+      ...playerCards,
+      <Card
+        key={`player-${playerCards.length + 1}`}
+        suit={hitCard[0]}
+        number={hitCard[1]}
+      />,
+    ]);
+    let playerHand = countCards(playerCards);
+
+    if (playerHand > 21) {
+      console.log("Sorry, you lost..");
+      setDisableBtn(true);
+    }
+  };
+
+  // MODAL FUNCTIONS
+  const modalHandler = (e) => {
+    setShowModal(!showModal);
+  };
+
+  let modal = (
+    <Modal isOpen={showModal} style={modalStyle}>
+      <h2>How much would you like to bet on this round?</h2>
+      <p>Your funds are below:</p>
+      <p>${props.bet}</p>
+      <form>
+        <input placeholder={"$"} />
+        <button onClick={modalHandler}>Lets Play!</button>
+        <button>Cancel</button>
+      </form>
+    </Modal>
+  );
+
+  const countCards = (arr) => {
+    let num = 0;
+    arr.map((card) => (num += card.props.number));
+    return num;
+  };
+
+  const isWinner = () => {
+    let playerHand = countCards(playerCards);
+    let dealerHand = countCards(dealerCards);
+
+    if (playerHand > dealerHand && playerHand <= 21) {
+      console.log("YOU WON!");
+      console.log("player:", playerHand);
+      console.log("dealer:", dealerHand);
+      setDisableBtn(true);
+    } else {
+      console.log("Sorry, dealer won...");
+      console.log("player:", playerHand);
+      console.log("dealer:", dealerHand);
+      setDisableBtn(true);
+    }
+  };
+
+  if (playerCards.length < 2) {
+    for (let i = 0; i < 2; i++) {
+      let temp = player1.deal();
+      setPlayerCards((playerCards) => [
+        ...playerCards,
+        <Card
+          key={`player-${i} + ${temp[0]}`}
+          suit={temp[0]}
+          number={temp[1]}
+        />,
+      ]);
+    }
+
+    for (let i = 0; i < 2; i++) {
+      let temp = dealer.deal();
+      setDealerCards((dealerCards) => [
+        ...dealerCards,
+        <Card
+          key={`dealer-${i} + ${temp[0]}`}
+          suit={temp[0]}
+          number={temp[1]}
+        />,
+      ]);
+    }
+  }
+
+  return (
+    <div className={classes.Table}>
+      {modal}
+      <Dealer />
+      <div>{dealerCards}</div>
+      {players}
+      <div>{playerCards}</div>
+      <h4 style={{ textAlign: "right" }}>Amount: ${props.bet}</h4>
+      <button
+        className={classes.Btn}
+        onClick={hitHandler}
+        disabled={disableBtn}
+      >
+        Hit
+      </button>
+      <button className={classes.Btn} onClick={isWinner} disabled={disableBtn}>
+        Pass
+      </button>
+      <button onClick={modalHandler}>Show Modal</button>
+    </div>
+  );
+};
+
+export default Table;
--- client/src/containers/Singleplayer/Singleplayer.js
+++ client/src/containers/Singleplayer/Singleplayer.js
@@ -1,38 +1,35 @@
-import { useState } from 'react';
+import { useState } from "react";
 import Title from "../../components/Title/Title";
 import Table from "../../components/Table/Table";
 
-
 const Singleplayer = (props) => {
-    const [bet, setBet] = useState(0);
-    const [gameStart, setGameStart] = useState(false);
+  const [bet, setBet] = useState(0);
+  const [gameStart, setGameStart] = useState(false);
 
+  const onBet = (event) => {
+    event.preventDefault();
+    setBet(event.target.value);
+  };
 
-    const onBet = (event) => {
-        event.preventDefault()
-        setBet(event.target.value)
-    };
+  const onGameStart = () => {
+    if (!bet) return console.log("You must bet first!");
+    setGameStart(<Table players={1} bet={bet} gameStart={true} />);
+  };
 
-    const onGameStart = () => {
-        if (!bet) return console.log("You must bet first!");
-        setGameStart((<Table players={1} bet={bet} gameStart={true}/>));
-        
-    };
+  let game = (
+    <div style={{ "text-align": "center", paddingTop: "10%" }}>
+      <h2>How much will you be playing with today?</h2>
+      <input type="number" onChange={onBet} placeholder={"$"} />
+      <button onClick={onGameStart}>Lets play!</button>
+    </div>
+  );
 
-    let game = (
-        <div>
-            <h2>How much would you like to bet today?</h2>
-            <input type="number" onChange={onBet}/>
-            <button onClick={onGameStart}>Lets play!</button>
-        </div>
-    );
-
-    return (
-        <div>
-            <Title title={"Single-Player"}/>
-            {gameStart ? gameStart:game}
-        </div>
-    );
+  return (
+    <div>
+      <Title title={"Single-Player"} />
+      {gameStart ? gameStart : game}
+    </div>
+  );
 };
 
-export default Singleplayer
+export default Singleplayer;
